05b96273d19ad1c43d016827687d116d45bbadf1
ryota-sb <ryota.sb.1206@gmail.com>
2022-07-30 20:00:10 +0900

    認証用メソッド追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    authorize_request = AuthorizationService.new(request.headers)
+    @current_user = authorize_request.current_user
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
 end
