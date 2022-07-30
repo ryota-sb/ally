94eb55b198c04391dbb8d154f497e747340706f3
ryota-sb <ryota.sb.1206@gmail.com>
2022-07-30 13:25:42 +0900

    ログアウト追加

--- frontend/app/pages/login.tsx
+++ frontend/app/pages/login.tsx
@@ -3,11 +3,20 @@ import { NextPage } from "next";
 import React from "react";
 
 const LoginPage: NextPage = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, logout } = useAuth0();
 
   return (
     <div>
-      {isAuthenticated ? <p>ログイン中</p> : <p>ログインしてください</p>}
+      {isAuthenticated ? (
+        <>
+          <p>ログイン中</p>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            ログアウト
+          </button>
+        </>
+      ) : (
+        <p>ログインしてください</p>
+      )}
     </div>
   );
 };
