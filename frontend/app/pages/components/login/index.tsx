06c4a2789ba934805dee7d9fdeaa23c4ca78bb17
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-02 19:17:13 +0900

    login component ディレクトリ変更

--- frontend/app/pages/components/login/index.tsx
+++ frontend/app/pages/components/login/index.tsx
@@ -3,12 +3,13 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
-import tokenState from "../recoil/atoms/tokenState";
+import tokenState from "../../../recoil/atoms/tokenState";
 
-const LoginPage: NextPage = () => {
+const Login: NextPage = () => {
   const { isAuthenticated, logout, getAccessTokenSilently } = useAuth0();
   const router = useRouter();
   const setToken = useSetRecoilState(tokenState);
+  const { loginWithRedirect } = useAuth0();
 
   // ログイン後にトークンを取得し、Recoilへ格納
   useEffect(() => {
@@ -27,10 +28,6 @@ const LoginPage: NextPage = () => {
     <div>
       {isAuthenticated ? (
         <>
-          <p>ログイン中</p>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            ログアウト
-          </button>
           <button
             onClick={() => {
               router.push("blog");
@@ -40,10 +37,23 @@ const LoginPage: NextPage = () => {
           </button>
         </>
       ) : (
-        <p>ログインしてください</p>
+        <div className="grid grid-cols-1">
+          <button
+            className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+            onClick={() => loginWithRedirect()}
+          >
+            Sign Up
+          </button>
+          <button
+            className="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+            onClick={() => loginWithRedirect()}
+          >
+            Sign In
+          </button>
+        </div>
       )}
     </div>
   );
 };
 
-export default LoginPage;
+export default Login;
