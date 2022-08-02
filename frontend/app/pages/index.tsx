fe3072fd9c65a4afc5b382472bae055ab76d2503
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-02 19:18:23 +0900

    main page 修正

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -1,16 +1,27 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useAuth0 } from "@auth0/auth0-react";
+import Header from "./components/header";
+import Login from "./components/login";
+
+// header token 確認用
+import { useRecoilValue } from "recoil";
+import tokenState from "../recoil/atoms/tokenState";
 
 const Home: NextPage = () => {
-  const { loginWithRedirect } = useAuth0();
+  const token = useRecoilValue(tokenState);
   return (
     <div>
       <Head>
         <title>Ally</title>
       </Head>
+      <Header />
       <main>
-        <button onClick={() => loginWithRedirect()}>ログイン</button>
+        <div className="truncate w-full max-w-sm px-4 py-3 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+          <p>{token}</p>
+        </div>
+        <div className="h-screen w-screen flex justify-center items-center">
+          <Login />
+        </div>
       </main>
     </div>
   );
