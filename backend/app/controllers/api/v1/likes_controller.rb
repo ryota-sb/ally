7ecec66850296c763d9b28b03eb349eee12af9c0
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-12 16:48:44 +0900

    syntax 修正

--- backend/app/controllers/api/v1/likes_controller.rb
+++ backend/app/controllers/api/v1/likes_controller.rb
@@ -6,12 +6,13 @@ class Api::V1::LikesController < ApplicationController
   def create
     is_matched = false
     
-    active_like = Like.find_of_initialize_by(like_params)
+    active_like = Like.find_or_initialize_by(like_params)
 
     if active_like.save
       render json: { status: 200, like: active_like, is_matched: is_matched }
     else
       render json: { status: 500, message: active_like.errors }
+    end
   end
   private
 
