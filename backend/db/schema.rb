fbe6c2fdcd29213a956447c09aac2e166a02ec50
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-12 16:31:45 +0900

    like model db 追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_041846) do
+ActiveRecord::Schema.define(version: 2022_08_12_021220) do
+
+  create_table "likes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "from_user_id", null: false
+    t.integer "to_user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
