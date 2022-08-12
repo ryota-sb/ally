4850a5e5cdc3acb0286c59e99fcc0315227c2882
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-12 16:29:21 +0900

    like model 追加 user model 関連付け

---
+++ backend/app/models/like.rb
@@ -0,0 +1,4 @@
+class Like < ApplicationRecord
+  belongs_to :from_user, class_name: 'User', foreign_key: :from_user_id
+  belongs_to :to_user, class_name: 'User', foreign_key: :to_user_id
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_one :profile
   has_many :posts, dependent: :destroy
-  
+
+  has_many :likes_from, class_name: 'Like', foreign_key: :from_user_id, dependent: :destroy
+  has_many :likes_to, class_name: 'Like', foreign_key: :to_user_id, dependent: :destroy
+  has_many :active_likes, through: :likes_from, source: :to_user
+  has_many :passive_likes, through: :likes_to, source: :from_user
+
   def self.from_token_payload(payload)
     find_by(sub: payload['sub']) || create!(sub: payload['sub'])
   end
