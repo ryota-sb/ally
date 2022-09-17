14436241d8ff43431482ff0f6e66bd89e144272c
ryota-sb <ryota.sb.1206@gmail.com>
2022-09-17 21:18:27 +0900

    login logout 時にユーザー情報を得るためのコントローラー追加

---
+++ backend/app/controllers/api/v1/secured_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SecuredController < ApplicationController
+  def login
+    render json: { 
+      status: 200,
+      message: "SUB: #{@current_user.sub}でログインしました。"
+      data: @current_user
+    }
+  end
+
+  def logout
+    render json: {
+      status: 200,
+      message: "SUB: #{@current_user.sub}でログアウトしました。"
+      data: @current_user
+    }
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'login', to: 'secured#login'
+      get 'logout', to: 'secured#logout'
       resources :posts
       resources :profiles, only: [:create, :update]
       resources :likes, only: [:index, :create]
