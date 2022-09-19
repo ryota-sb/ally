f51c13c008243b1e092a198affe86923b7df42f8
ryota-sb <ryota.sb.1206@gmail.com>
2022-09-19 23:29:11 +0900

    構文エラー修正

--- backend/app/controllers/api/v1/secured_controller.rb
+++ backend/app/controllers/api/v1/secured_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SecuredController < ApplicationController
   def login
     render json: { 
       status: 200,
-      message: "SUB: #{@current_user.sub}でログインしました。"
+      message: "SUB: #{@current_user.sub}でログインしました。",
       data: @current_user
     }
   end
@@ -10,7 +10,7 @@ class Api::V1::SecuredController < ApplicationController
   def logout
     render json: {
       status: 200,
-      message: "SUB: #{@current_user.sub}でログアウトしました。"
+      message: "SUB: #{@current_user.sub}でログアウトしました。",
       data: @current_user
     }
   end
