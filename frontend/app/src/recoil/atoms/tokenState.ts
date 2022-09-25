e805c26a83b0b6e6695278a7f798ba956efb7764
ryota-sb <ryota.sb.1206@gmail.com>
2022-09-25 19:10:35 +0900

    recoil value の永続化

--- frontend/app/src/recoil/atoms/tokenState.ts
+++ frontend/app/src/recoil/atoms/tokenState.ts
@@ -1,8 +1,12 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const tokenState = atom({
   key: "tokenState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export default tokenState;
