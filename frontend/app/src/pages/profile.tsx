1fba9d4e89ee080fbc5675566ef2a08ee4b3b9cb
ryota-sb <ryota.sb.1206@gmail.com>
2022-09-27 18:41:18 +0900

    style調整

--- frontend/app/src/pages/profile.tsx
+++ frontend/app/src/pages/profile.tsx
@@ -12,7 +12,12 @@ type formState = {
 type Props = {};
 
 const Profile: NextPage<Props> = () => {
-  const { register, handleSubmit, reset } = useForm<formState>({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<formState>({
     mode: "onChange",
     defaultValues: {
       gender: "",
@@ -28,19 +33,94 @@ const Profile: NextPage<Props> = () => {
   });
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className="">
-          <span>Gender</span>
-          <input {...register("gender", { required: true })} />
-          <span>discord_id</span>
-          <input {...register("discord_id")} />
-          <span>game_rank</span>
-          <input {...register("game_rank")} />
-          <span>game_category</span>
-          <input {...register("game_category")} />
-          <span>image</span>
-          <input {...register("image")} />
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <h1 className="mb-10 text-3xl">プロフィール設定</h1>
+      <form className="w-full max-w-sm" onSubmit={onSubmit}>
+        <div className="mb-6">
+          {/* gender */}
+          <div className="mb-6 flex">
+            <div className="flex w-1/3 items-center">
+              <label className="mb-1 pr-4 font-bold text-gray-500">
+                Gender
+              </label>
+            </div>
+            <div className="w-2/3">
+              <input
+                {...register("gender", { required: true })}
+                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+              />
+              {errors.gender && (
+                <div className="text-red-500">入力が必須の項目です。</div>
+              )}
+            </div>
+          </div>
+          {/* discord_id */}
+          <div className="mb-6 flex">
+            <div className="flex w-1/3 items-center">
+              <label className="mb-1 pr-4 font-bold text-gray-500">
+                discord_id
+              </label>
+            </div>
+            <div className="w-2/3">
+              <input
+                {...register("discord_id")}
+                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+              />
+            </div>
+          </div>
+          {/* game_rank */}
+          <div className="mb-6 flex">
+            <div className="flex w-1/3 items-center">
+              <label className="mb-1 pr-4 font-bold text-gray-500">
+                game_rank
+              </label>
+            </div>
+            <div className="w-2/3">
+              <input
+                {...register("game_rank")}
+                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+              />
+            </div>
+          </div>
+          {/* game_category */}
+          <div className="mb-6 flex">
+            <div className="flex w-1/3 items-center">
+              <label className="mb-1 pr-4 font-bold text-gray-500">
+                game_category
+              </label>
+            </div>
+            <div className="w-2/3">
+              <input
+                {...register("game_category")}
+                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+              />
+            </div>
+          </div>
+          {/* image */}
+          <div className="mb-6 flex">
+            <div className="flex w-1/3 items-center">
+              <label className="mb-1 pr-4 font-bold text-gray-500 md:mb-0 md:text-right">
+                image
+              </label>
+            </div>
+            <div className="w-2/3">
+              <input
+                {...register("image")}
+                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="w-1/3"></div>
+            <div className="w-2/3">
+              <button
+                className="focus:shadow-outline rounded bg-purple-500 py-2 px-4 font-bold text-white shadow hover:bg-purple-400 focus:outline-none"
+                type="submit"
+              >
+                プロフィール登録
+              </button>
+            </div>
+          </div>
         </div>
       </form>
     </div>
