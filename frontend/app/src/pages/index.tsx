70b5069b58e2f07fd99498039a3a474d685c5446
ryota-sb <ryota.sb.1206@gmail.com>
2022-10-03 18:27:49 +0900

    認証による画面表示の条件分岐追加

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header";
-import Login from "../components/login";
+import Header from "../components/Header/Header";
+import Login from "../components/Login/Login";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 // header token 確認用
 import { useRecoilValue } from "recoil";
 import tokenState from "../recoil/atoms/tokenState";
 
 const Home: NextPage = () => {
+  const { isAuthenticated } = useAuth0();
   const token = useRecoilValue(tokenState);
   return (
     <div>
@@ -16,12 +19,15 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main>
-        <div className="mx-auto w-full max-w-sm truncate rounded-md bg-white px-4 py-3 shadow-md dark:bg-gray-800">
-          <p>{token}</p>
-        </div>
-        <div className="flex h-screen w-screen items-center justify-center">
-          <Login />
-        </div>
+        {isAuthenticated ? (
+          <div className="mx-auto w-full max-w-sm truncate rounded-md bg-white px-4 py-3 shadow-md dark:bg-gray-800">
+            <p>{token}</p>
+          </div>
+        ) : (
+          <div className="flex h-screen w-screen items-center justify-center">
+            <Login />
+          </div>
+        )}
       </main>
     </div>
   );
