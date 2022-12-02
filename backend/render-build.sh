62d7427a65f4f6a5356a7ba25d2e355878d26dd4
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-02 20:23:34 +0900

    不要部分コメントアウト

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
-# exit on error
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
 
-set -o errexit
-
-bundle install
-bundle exec rake db:migrate
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
