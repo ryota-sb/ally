ddd886ffeaedf550d62141e8f381c70f06ac56f7
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-03 19:58:08 +0900

    微修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,8 @@ rm -f /app/tmp/pids/server.pid
 exec "$@"
 
 # 本番環境の時のみマイグレーションを実行
-if [ "$RAILS_ENV" == "production" ]; then
+if [ "$RAILS_ENV" = "production" ]; then
+bundle exec rails assets:clobber
+bundle exec rails assets:precompile
 bundle exec rails db:migrate
 fi
