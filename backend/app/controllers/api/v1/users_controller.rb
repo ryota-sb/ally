8cdb05462527452d99af3958623f61130a76d40c
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-09 22:38:03 +0900

    ランダムで一つのユーザープロフィールを取得

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authorize_request, only: :index
 
   def index
-    users = User.all
-    render json: users
+    # ログインしているユーザー以外のプロフィールレコードを1つランダムで取得する
+    profile = Profile.where.not(user_id: 1).order(Arel.sql('RANDOM()')).limit(1)
+    # user = User.find_by(id: profile[0].user_id)
+    render json: { data: profile[0] }
   end
 end
