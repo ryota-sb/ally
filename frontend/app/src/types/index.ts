7c0f0c6752f0a4c2ac01f7d23212f47030b313cf
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-09 23:07:25 +0900

    型定義 追加

--- frontend/app/src/types/index.ts
+++ frontend/app/src/types/index.ts
@@ -1,7 +1,16 @@
-export type ProfileState = {
+export type Profile = {
+  nickname: string;
   gender: string;
-  discord_id: string;
+  discord_id?: string;
   game_rank?: string;
   game_category?: string;
   image?: string;
+};
+
+export type User = {
+  id: number;
+  sub: string;
+  createdAt: Date;
+  updatedAt: Date;
+  profile: Profile;
 };
