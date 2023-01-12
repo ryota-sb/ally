92871addd697dd66372080a5093a5ad9cece1086
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-12 21:50:39 +0900

    ディレクトリの変更

--- frontend/app/src/components/Header.tsx
+++ frontend/app/src/components/Header.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useSetRecoilState } from "recoil";
-import tokenState from "../../recoil/atoms/tokenState";
-import userState from "../../recoil/atoms/userState";
+import tokenState from "../recoil/atoms/tokenState";
+import userState from "../recoil/atoms/userState";
 
 const Header: FC = () => {
   const { isAuthenticated, logout } = useAuth0();
