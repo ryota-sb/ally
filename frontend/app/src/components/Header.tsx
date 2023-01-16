e16faf720e054dd966ac31fe590b6ff185cecdce
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-16 19:53:11 +0900

    headerコンポーネント 修正

--- frontend/app/src/components/Header.tsx
+++ frontend/app/src/components/Header.tsx
@@ -32,16 +32,22 @@ const Header: FC = () => {
               <div className="flex-1 md:flex md:items-center md:justify-between">
                 <div className="-mx-4 flex flex-col md:mx-8 md:flex-row md:items-center">
                   <a
-                    href="#"
+                    href="/users"
                     className="mx-2 mt-2 transform rounded-md px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 hover:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 md:mt-0"
                   >
-                    Profile
+                    探す
                   </a>
                   <a
                     href="#"
                     className="mx-2 mt-2 transform rounded-md px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 hover:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 md:mt-0"
                   >
-                    <button onClick={() => userLogout()}>Logout</button>
+                    プロフィール
+                  </a>
+                  <a
+                    href="#"
+                    className="mx-2 mt-2 transform rounded-md px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 hover:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 md:mt-0"
+                  >
+                    <button onClick={() => userLogout()}>ログアウト</button>
                   </a>
                 </div>
               </div>
@@ -52,7 +58,7 @@ const Header: FC = () => {
                     href="#"
                     className="mx-2 mt-2 transform rounded-md px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 hover:bg-gray-300 dark:text-gray-200 dark:hover:bg-gray-700 md:mt-0"
                   >
-                    Home
+                    ホーム
                   </a>
                 </div>
               </div>
