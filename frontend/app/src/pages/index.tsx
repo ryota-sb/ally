5719d82d48b99c19ebc32e87b79a442d37e5c7ae
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-16 20:01:18 +0900

    不要な処理削除 データ取得待機中は、Loadingページを表示するように変更

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,34 +1,39 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header/Header";
-import Login from "../components/Login/Login";
+
+import Loading from "pages/loading";
+import Layout from "components/Layout";
+import Login from "components/Login";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 // header token 確認用
 import { useRecoilValue } from "recoil";
-import tokenState from "../recoil/atoms/tokenState";
+import tokenState from "recoil/atoms/tokenState";
 
 const Home: NextPage = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
   const token = useRecoilValue(tokenState);
   return (
     <div>
       <Head>
         <title>Ally</title>
       </Head>
-      <Header />
-      <main>
-        {isAuthenticated ? (
-          <div className="mx-auto w-full max-w-sm truncate rounded-md bg-white px-4 py-3 shadow-md dark:bg-gray-800">
-            <p>{token}</p>
-          </div>
-        ) : (
-          <div className="flex h-screen w-screen items-center justify-center">
-            <Login />
-          </div>
-        )}
-      </main>
+      <Layout>
+        <main>
+          {isAuthenticated ? (
+            <div className="mx-auto w-full max-w-sm truncate rounded-md bg-white px-4 py-3 shadow-md dark:bg-gray-800">
+              <p>{token}</p>
+            </div>
+          ) : isLoading ? (
+            <Loading />
+          ) : (
+            <div className="flex h-screen w-screen items-center justify-center">
+              <Login />
+            </div>
+          )}
+        </main>
+      </Layout>
     </div>
   );
 };
