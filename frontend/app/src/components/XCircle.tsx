a4574a0b3f1f38f3560dcec5dc3e71872524e506
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-16 21:17:30 +0900

    ボタンコンポーネント追加

---
+++ frontend/app/src/components/XCircle.tsx
@@ -0,0 +1,18 @@
+const XCircle = ({ size = 100, color = "#2598d5" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="1"
+    strokeLinecap="square"
+    strokeLinejoin="round"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="15" y1="9" x2="9" y2="15"></line>
+    <line x1="9" y1="9" x2="15" y2="15"></line>
+  </svg>
+);
+export default XCircle;
