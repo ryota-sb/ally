d64ed48f6bb1f9157f45ee0547081d74055cdb4d
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-21 19:46:40 +0900

    外部キー制約を追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_09_114345) do
+ActiveRecord::Schema.define(version: 2023_01_21_104305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,11 @@ ActiveRecord::Schema.define(version: 2023_01_09_114345) do
     t.string "game_category", null: false
     t.string "discord_id"
     t.integer "gender", default: 0, null: false
-    t.bigint "user_id", null: false
     t.string "image"
     t.string "nickname", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -41,4 +41,5 @@ ActiveRecord::Schema.define(version: 2023_01_09_114345) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
