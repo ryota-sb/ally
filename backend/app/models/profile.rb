d924a631e17c85b36ba045f89a8e5b531511394e
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-22 22:22:49 +0900

    Profileモデルの外部キーにユニーク制約付与

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,8 +1,9 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :user_id, uniqueness: true
+  
+  mount_uploader :image, ImageUploader
 
   validates :gender, { presence: true }
-
-  mount_uploader :image, ImageUploader
   enum gender: { man: 0, woman: 1 }
 end
