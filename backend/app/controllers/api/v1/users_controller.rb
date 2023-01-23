524f35ca62c2312fadc85b30fb1209845edf0645
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-23 21:26:34 +0900

    不要コード削除

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  # skip_before_action :authorize_request
-
   def index
     # ログインしているユーザー以外のプロフィールレコードを1つランダムで取得する
     profile = Profile.where.not(user_id: @current_user.id).order(Arel.sql('RANDOM()')).limit(1)
