55a9880ae7d53422d98b8e223e8874704a75daaf
ryota-sb <ryota.sb.1206@gmail.com>
2023-01-29 18:39:46 +0900

    微修正

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const redirectUri = `${process.env["NEXT_PUBLIC_CALLBACK_URL"]}`;
+
   return (
     <Auth0Provider
       domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]!}
