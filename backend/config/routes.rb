d552cf9ed6d2120a70299c67cf2b45716847d5c9
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-06 21:19:06 +0900

    ルーティングの追記

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
       get 'login', to: 'secured#login'
       get 'logout', to: 'secured#logout'
       resources :tests, only: [:index]
-      resources :users, only: [:index]
       resources :posts
+      resources :users, only: [:index]
       resources :profiles, only: [:index, :show, :create, :update]
-      resources :likes, only: [:index, :create]
+      resources :likes, only: [:index, :create],
+      resources :chat_rooms, only: [:index, :show]
+      resources :messages, only: [:create]
     end
   end
 end
