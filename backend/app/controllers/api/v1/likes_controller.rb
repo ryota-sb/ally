27e4914ad50943cc3de8dbe7e1ddc8d4055d2bea
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-06 21:16:55 +0900

    マッチング時に、ルームを作成する処理追加

--- backend/app/controllers/api/v1/likes_controller.rb
+++ backend/app/controllers/api/v1/likes_controller.rb
@@ -7,13 +7,34 @@ class Api::V1::LikesController < ApplicationController
     is_matched = false
     
     active_like = Like.find_or_initialize_by(like_params)
+    passive_like = Like.find_by(
+      from_user_id: active_like.to_user_id,
+      to_user_id: active_like.from_user_id
+    )
+
+    if passive_like
+      chat_room = ChatRoom.create
+
+      ChatRoomUser.find_or_create_by(
+        chat_room_id: chat_room.id,
+        user_id: active_like.from_user_id
+      )
+
+      ChatRoomUser.find_or_create_by(
+        chat_room_id: chat_room.id,
+        user_id: passive_like.from_user_id
+      )
+
+      is_matched = true
+    end
 
     if active_like.save
       render json: { status: 200, like: active_like, is_matched: is_matched }
     else
-      render json: { status: 500, message: active_like.errors }
+      render json: { status: 500, message: "作成に失敗しました" }
     end
   end
+
   private
 
   def like_params
