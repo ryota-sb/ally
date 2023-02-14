b98dac89de9600f5a7783a0cd05ac8ea710af6c2
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-14 23:41:11 +0900

    ユーザーを取得するためのuseSWRのカスタムフック作成

---
+++ frontend/app/src/hooks/api/user.ts
@@ -0,0 +1,45 @@
+import useSWR from "swr";
+import { User } from "types";
+
+import { useRecoilValue } from "recoil";
+import tokenState from "recoil/atoms/tokenState";
+
+class UserFetcher {
+  // ランダムでユーザーを1人取得
+  static getRandomUser() {
+    const token = useRecoilValue(tokenState);
+    const { data, error } = useSWR<User>(
+      "http://localhost:3000/api/v1/random_user",
+      (url) =>
+        fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then(
+          (res) => res.json()
+        )
+    );
+
+    return {
+      user: data,
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+
+  // 渡されたIDのユーザーを取得
+  static getUser(id: number) {
+    const token = useRecoilValue(tokenState);
+    const { data, error } = useSWR<User>(
+      `http://localhost:3000/api/v1/users/${id}`,
+      (url) =>
+        fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then(
+          (res) => res.json()
+        )
+    );
+
+    return {
+      user: data,
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+}
+
+export default UserFetcher;
