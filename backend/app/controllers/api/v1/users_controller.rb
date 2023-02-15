4f09f2f8119c4e9394193463893c272f20a954a7
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-15 22:23:10 +0900

    ランダムでユーザーを取得するアクションとshowアクションを追加

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::UsersController < ApplicationController
-  def index
-    # ログインしているユーザー以外のプロフィールレコードを1つランダムで取得する
-    profile = Profile.where.not(user_id: @current_user.id).order(Arel.sql('RANDOM()')).limit(1)
-    render json: profile[0]
+  
+  # ログインしているユーザー以外でプロフィールを登録済のユーザーを1つランダムで取得
+  def random_user_with_profile
+    user = User.joins(:profile).where.not(id: @current_user.id).order(Arel.sql('RANDOM()')).limit(1)
+    profile = user[0].profile
+    render json: { user: user[0], profile: profile }
+  end
+
+  def show
+    user = User.find(params[:id])
+    profile = user.profile
+    render json: { user: user, profile: profile }
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
       get 'logout', to: 'secured#logout'
       resources :tests, only: [:index]
       resources :posts
-      resources :users, only: [:index]
+
+      get 'random_user_with_profile', to: 'users#random_user_with_profile'
+      resources :users, only: [:show]
       resources :profiles, only: [:index, :show, :create, :update]
       resources :likes, only: [:index, :create]
       resources :chat_rooms, only: [:index, :show]
