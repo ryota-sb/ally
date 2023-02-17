fdc1ce6e420455c1314ee7e09c0bcdf0f7f8a8c5
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-17 19:18:33 +0900

    不要アクション削除

--- backend/app/controllers/api/v1/profiles_controller.rb
+++ backend/app/controllers/api/v1/profiles_controller.rb
@@ -1,16 +1,5 @@
 class Api::V1::ProfilesController < ApplicationController
-  skip_before_action :authorize_request, only: [:index, :show]
-
-  def index
-    profiles = Profile.all
-    render json: profiles
-  end
-
-  def show
-    profile = Profile.find(params[:id])
-    render json: profile
-  end
-
+  
   def create
     profile = Profile.new(profile_params)
     profile.user_id = @current_user.id
