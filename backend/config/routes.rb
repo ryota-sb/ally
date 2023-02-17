494e1b844070593cfdd741671a6a6b8b88d8ea6c
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-17 19:18:50 +0900

    不要ルート削除

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
       get 'login', to: 'secured#login'
       get 'logout', to: 'secured#logout'
       resources :tests, only: [:index]
-      resources :posts
 
       get 'random_user_with_profile', to: 'users#random_user_with_profile'
-      resources :users, only: [:show]
-      resources :profiles, only: [:index, :show, :create, :update]
+      resources :users, only: :show
+      resources :profiles, only: [:create, :update]
       resources :likes, only: [:index, :create]
       resources :chat_rooms, only: [:index, :show]
-      resources :messages, only: [:create]
+      resources :messages, only: :create
     end
   end
 end
