927d2c36de94e5cd7a6a52fefd681023281caaaa
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-19 21:32:44 +0900

    チャットルーム一覧のリファクタリング

--- frontend/app/src/pages/chatRooms/index.tsx
+++ frontend/app/src/pages/chatRooms/index.tsx
@@ -1,60 +1,58 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
-import useSWR from "swr";
-
-import { ChatRoom } from "types/index";
+import ChatRoomFetcher from "hooks/api/chat_room";
 
 import Loading from "pages/loading";
 import Layout from "components/Layout";
 
-import { useRecoilValue } from "recoil";
-import tokenState from "recoil/atoms/tokenState";
-
 const ChatRooms: NextPage = () => {
-  const token = useRecoilValue(tokenState);
+  const { chat_rooms, isLoading, isError } = ChatRoomFetcher.getChatRooms();
 
-  const fetcher = (url: string): Promise<ChatRoom[]> =>
-    fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then((res) =>
-      res.json()
-    );
+  const getValue = () => {
+    console.log(chat_rooms);
+  };
 
-  const { data, error } = useSWR(
-    "http://localhost:3000/api/v1/chat_rooms",
-    fetcher
-  );
-
-  if (error) return <div>An error has occurred.</div>;
-  if (!data) return <Loading />;
+  if (isLoading) return <Loading />;
+  if (isError) return <div>error...</div>;
 
   return (
     <Layout>
-      {data.length > 0 ? (
+      {chat_rooms && chat_rooms.length > 0 ? (
         <div className="flex min-h-screen flex-col items-center bg-gray-100">
           <h1 className="p-10 text-4xl">Chat Rooms</h1>
-          {data.map((room) => (
-            <div className="flex w-1/2 flex-col divide-y divide-gray-200">
-              <div className="flex gap-6 rounded-2xl bg-white p-8">
-                <Image
-                  src="/113120.jpeg"
-                  alt="サンプル画像"
-                  width={100}
-                  height={100}
-                  className="rounded-full object-cover"
-                />
+          {chat_rooms.map((chat_room) => (
+            <div
+              key={chat_room.chat_room.id}
+              className="flex w-1/2 flex-col divide-y divide-gray-200"
+            >
+              <Link href={`/users/${chat_room.other_user.id}`}>
+                <div className="m-4 flex gap-6 rounded-2xl bg-white p-8">
+                  <Image
+                    src={chat_room.other_user_profile.image?.url!}
+                    alt="サンプル画像"
+                    width={100}
+                    height={100}
+                    className="rounded-full object-cover"
+                  />
 
-                <div className="flex flex-col items-start justify-center gap-3">
-                  <h1 className="text-3xl">
-                    {room.other_user_profile.nickname}
-                  </h1>
-                  <h1>{room.other_user_profile.game_category}</h1>
+                  <div className="flex flex-col items-start justify-center gap-3">
+                    <h1 className="text-3xl">
+                      {chat_room.other_user_profile.nickname}
+                    </h1>
+                    <h1>{chat_room.other_user_profile.game_category}</h1>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
+          <button onClick={() => getValue()}>console</button>
         </div>
       ) : (
-        <div>現在、マッチングした相手がいません。</div>
+        <div className="flex h-screen w-screen items-center justify-center">
+          <h1 className="text-xl">現在、マッチングした相手がいません。</h1>
+        </div>
       )}
     </Layout>
   );
