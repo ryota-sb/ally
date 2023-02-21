c7f3ca7192f6e5213c6e3029e77a13223c4d97ed
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-21 20:55:03 +0900

    必須項目のバリデーションを追加

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,9 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  validates :user_id, uniqueness: true
-  
+
   mount_uploader :image, ImageUploader
 
-  validates :gender, { presence: true }
+  validates :user_id, uniqueness: true
+  validates :nickname, presence: true
+  validates :gender, presence: true
+  validates :game_category, presence: true
   enum gender: { man: 0, woman: 1 }
 end
