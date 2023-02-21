d88f48e1f437da0c9d00648162a5e64bfef5c745
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-21 20:54:33 +0900

    postsを削除、chat_roomをchat_roomsに修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
-  has_many :posts, dependent: :destroy
 
   has_many :likes_from, class_name: 'Like', foreign_key: :from_user_id, dependent: :destroy
   has_many :likes_to, class_name: 'Like', foreign_key: :to_user_id, dependent: :destroy
@@ -8,7 +7,7 @@ class User < ApplicationRecord
   has_many :passive_likes, through: :likes_to, source: :from_user
 
   has_many :chat_room_users
-  has_many :chat_room, through: :chat_room_users
+  has_many :chat_rooms, through: :chat_room_users
 
   has_many :messages
 
