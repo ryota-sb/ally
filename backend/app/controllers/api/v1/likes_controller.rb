67a8733613ee43435cedfad32899df6bff2db9b4
ryota-sb <ryota.sb.1206@gmail.com>
2023-02-21 20:50:56 +0900

    is_likeカラムを追加し、ユーザーがいいねを押したのか良くないを押したのかを管理できるように変更

--- backend/app/controllers/api/v1/likes_controller.rb
+++ backend/app/controllers/api/v1/likes_controller.rb
@@ -6,12 +6,16 @@ class Api::V1::LikesController < ApplicationController
   def create
     is_matched = false
     
+    # 自分がいいねした場合のレコード作成
     active_like = Like.find_or_initialize_by(like_params)
+    # 相手が自分のことをいいねしているかのレコードを探す
     passive_like = Like.find_by(
       from_user_id: active_like.to_user_id,
-      to_user_id: active_like.from_user_id
+      to_user_id: active_like.from_user_id,
+      is_like: true
     )
 
+    # 相手が自分をいいねしていれば、ルーム作成し、is_matchedのフラグをtrueにする
     if passive_like
       chat_room = ChatRoom.create
 
@@ -24,7 +28,7 @@ class Api::V1::LikesController < ApplicationController
         chat_room_id: chat_room.id,
         user_id: passive_like.from_user_id
       )
-
+      
       is_matched = true
     end
 
@@ -38,6 +42,6 @@ class Api::V1::LikesController < ApplicationController
   private
 
   def like_params
-    params.permit(:from_user_id, :to_user_id)
+    params.permit(:from_user_id, :to_user_id, :is_like)
   end
 end
