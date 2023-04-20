2307761cdef733f68da0bafe943c3aa26242ce69
ryota-sb <ryota.sb.1206@gmail.com>
2023-04-20 20:14:21 +0900

    messageコントローラー完成

--- backend/app/controllers/api/v1/messages_controller.rb
+++ backend/app/controllers/api/v1/messages_controller.rb
@@ -1,17 +1,20 @@
 class Api::V1::MessagesController < ApplicationController
+
+  # POST /api/v1/messages
   def create
     message = Message.new(message_params)
+    message.chat_room_id = params[:message][:chat_room_id]
 
     if message.save
       render json: { status: 200, message: message }
     else
-      render json: { status: 500, message: "作成に失敗しました" }
+      render json: { status: 500, message: message.errors.full_messages.join(", ") }
     end
   end
 
   private
 
   def message_params
-    params.permit(:user_id, :chat_room_id, :content)
+    params.require(:message).permit(:chat_room_id, :content).merge(user_id: @current_user.id)
   end
 end
