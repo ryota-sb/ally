7202035cbc7ee3a56de4325231571663513eb0e4
ryota-sb <ryota.sb.1206@gmail.com>
2023-04-22 23:30:07 +0900

    クラスではなく、関数に変更

--- frontend/app/src/hooks/api/chat_room.ts
+++ frontend/app/src/hooks/api/chat_room.ts
@@ -11,42 +11,41 @@ import { ChatRoomData } from "types";
 import { useRecoilValue } from "recoil";
 import tokenState from "recoil/atoms/tokenState";
 
-class ChatRoomFetcher {
-  // 全てのチャットルーム取得
-  static getChatRooms() {
-    const token = useRecoilValue(tokenState);
-    const { data, error } = useSWR<ChatRoomData[]>(
-      `${getBasePath()}/api/v1/chat_rooms`,
-      (url) =>
-        fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then(
-          (res) => res.json()
-        )
-    );
+// 全てのチャットルーム取得
+const getChatRooms = () => {
+  const token = useRecoilValue(tokenState);
+  const { data, error } = useSWR<ChatRoomData[]>(
+    `${getBasePath()}/api/v1/chat_rooms`,
+    (url) =>
+      fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then(
+        (res) => res.json()
+      )
+  );
 
-    return {
-      chatRooms: data,
-      isLoading: !error && !data,
-      isError: error,
-    };
-  }
+  return {
+    chatRooms: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
 
-  // 渡されたIDのチャットルームを取得
-  static getChatRoom(id: number) {
-    const token = useRecoilValue(tokenState);
-    const { data, error } = useSWR<ChatRoomData>(
-      `${getBasePath()}/api/v1/chat_rooms/${id}`,
-      (url) =>
-        fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then(
-          (res) => res.json()
-        )
-    );
+// 渡されたIDのチャットルームを取得
+const getChatRoom = (id: number) => {
+  const token = useRecoilValue(tokenState);
+  const { data, error, mutate } = useSWR<ChatRoomData>(
+    `${getBasePath()}/api/v1/chat_rooms/${id}`,
+    (url) =>
+      fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then(
+        (res) => res.json()
+      )
+  );
 
-    return {
-      chatRoom: data,
-      isLoading: !error && !data,
-      isError: error,
-    };
-  }
-}
+  return {
+    chatRoom: data,
+    isLoading: !error && !data,
+    isError: error,
+    mutate,
+  };
+};
 
-export default ChatRoomFetcher;
+export { getChatRooms, getChatRoom };
