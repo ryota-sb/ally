b18369d018fa1c0bbab4ceab5cdb64b6c8b44859
ryota-sb <ryota.sb.1206@gmail.com>
2023-06-21 22:06:27 +0900

    レイアウトとブロック変数名変更

--- frontend/app/src/pages/chatRooms/index.tsx
+++ frontend/app/src/pages/chatRooms/index.tsx
@@ -18,33 +18,32 @@ const ChatRooms: NextPage = () => {
   return (
     <Layout>
       {chatRooms && chatRooms.length > 0 ? (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100">
-          <h1 className="p-10 text-4xl">Chat Rooms</h1>
-          {chatRooms.map((chat_room) => (
-            <div
-              key={chat_room.chatRoom.id}
-              className="flex w-1/2 flex-col divide-y divide-gray-200"
-            >
-              <Link href={`/chatRooms/${chat_room.chatRoom.id}`}>
-                <div className="m-4 flex cursor-pointer gap-6 rounded-2xl bg-white p-8">
-                  <Image
-                    src={chat_room.otherUserProfile.image?.url!}
-                    alt="サンプル画像"
-                    width={100}
-                    height={100}
-                    className="rounded-full object-cover"
-                  />
+        <div className="min-h-screen bg-gray-100">
+          <div className="mx-auto max-w-2xl">
+            <h1 className="p-10 text-center text-4xl">Chat Rooms</h1>
+            {chatRooms.map((chatRoomItem) => (
+              <div key={chatRoomItem.chatRoom.id} className="flex flex-col">
+                <Link href={`/chatRooms/${chatRoomItem.chatRoom.id}`}>
+                  <div className="m-4 flex cursor-pointer gap-6 rounded-2xl bg-white p-8">
+                    <Image
+                      src={chatRoomItem.otherUser.profile?.image?.url!}
+                      alt={chatRoomItem.otherUser.profile?.nickname}
+                      width={100}
+                      height={100}
+                      className="rounded-full object-cover"
+                    />
 
-                  <div className="flex flex-col items-start justify-center gap-3">
-                    <h1 className="text-3xl">
-                      {chat_room.otherUserProfile.nickname}
-                    </h1>
-                    <h1>{chat_room.otherUserProfile.gameCategory}</h1>
+                    <div className="flex flex-col items-start justify-center gap-3">
+                      <h2 className="text-3xl">
+                        {chatRoomItem.otherUser.profile?.nickname}
+                      </h2>
+                      <h2>{chatRoomItem.otherUser.profile?.gameCategory}</h2>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          ))}
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <div className="flex h-screen w-screen items-center justify-center">
