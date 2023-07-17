56ebcb0f734af45bae186fa1cf5a6c5ab2dfa0ef
ryota-sb <ryota.sb.1206@gmail.com>
2023-07-17 22:45:40 +0900

    ボタンスタイル修正

--- frontend/app/src/pages/likes/index.tsx
+++ frontend/app/src/pages/likes/index.tsx
@@ -45,32 +45,30 @@ const Likes: NextPage = () => {
     return (
       <>
         {likes && (
-          <div className="min-h-screen bg-gray-100">
-            <div className="mx-auto max-w-2xl">
-              <h1 className="p-10 text-center text-4xl">
-                {isActiveLikes
-                  ? "いいねしたユーザー"
-                  : "いいねしてくれたユーザー"}
-              </h1>
-              {likes.map((likeUser) => (
-                <div key={likeUser.id} className="flex flex-col">
-                  <div className="m-4 flex cursor-pointer gap-6 rounded-2xl bg-white p-8">
-                    <Image
-                      src={likeUser.profile?.image?.url!}
-                      alt={likeUser.profile?.nickname}
-                      width={100}
-                      height={100}
-                      className="rounded-full object-cover"
-                    />
+          <div className="mx-auto max-w-2xl">
+            <h1 className="p-10 text-center text-4xl">
+              {isActiveLikes
+                ? "いいねしたユーザー"
+                : "いいねしてくれたユーザー"}
+            </h1>
+            {likes.map((likeUser) => (
+              <div key={likeUser.id} className="flex flex-col">
+                <div className="m-4 flex cursor-pointer gap-6 rounded-2xl bg-white p-8">
+                  <Image
+                    src={likeUser.profile?.image?.url!}
+                    alt={likeUser.profile?.nickname}
+                    width={100}
+                    height={100}
+                    className="rounded-full object-cover"
+                  />
 
-                    <div className="flex flex-col items-start justify-center gap-3">
-                      <h2 className="text-3xl">{likeUser.profile?.nickname}</h2>
-                      <h2>{likeUser.profile?.gameCategory}</h2>
-                    </div>
+                  <div className="flex flex-col items-start justify-center gap-3">
+                    <h2 className="text-3xl">{likeUser.profile?.nickname}</h2>
+                    <h2>{likeUser.profile?.gameCategory}</h2>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         )}
       </>
@@ -79,11 +77,24 @@ const Likes: NextPage = () => {
 
   return (
     <Layout>
-      <button onClick={() => setIsActiveLikes(true)}>いいねしたユーザー</button>
-      <button onClick={() => setIsActiveLikes(false)}>
-        いいねしてくれたユーザー
-      </button>
-      {<>{renderLikeUsers()}</>}
+      <div className="min-h-screen bg-gray-100">
+        <div className="grid grid-cols-2 bg-gray-300">
+          <button
+            onClick={() => setIsActiveLikes(true)}
+            className={isActiveLikes ? "bg-gray-400" : ""}
+          >
+            いいねしたユーザー
+          </button>
+          <button
+            onClick={() => setIsActiveLikes(false)}
+            className={!isActiveLikes ? "bg-gray-400" : ""}
+          >
+            いいねしてくれたユーザー
+          </button>
+        </div>
+
+        {<>{renderLikeUsers()}</>}
+      </div>
     </Layout>
   );
 };
