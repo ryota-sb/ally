aa4e1f0dd0fe4a0bf79cb1a4b2ad76d9dd8b2e87
ryota-sb <ryota.sb.1206@gmail.com>
2023-07-20 23:38:02 +0900

    fix scope

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /api/v1/random_user_with_profile
   def random_user_with_profile
-    other_user = User.excluding_user(@current_user).unliked_by(@current_user).order(Arel.sql('RANDOM()')).first
+    other_user = User.unliked_guest_users(@current_user).order(Arel.sql('RANDOM()')).first
 
     if other_user
       render json: other_user
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
 
-  has_many :likes_from, -> { liked }, class_name: 'Like', foreign_key: :from_user_id, dependent: :destroy
+  has_many :likes_from, class_name: 'Like', foreign_key: :from_user_id, dependent: :destroy
   has_many :likes_to, class_name: 'Like', foreign_key: :to_user_id, dependent: :destroy
   has_many :active_likes, through: :likes_from, source: :to_user
   has_many :passive_likes, through: :likes_to, source: :from_user
@@ -11,8 +11,15 @@ class User < ApplicationRecord
 
   has_many :messages
 
-  scope :excluding_user, ->(user) { where.not(id: user.id) } # 利用者（ログインユーザー）以外を取得
-  scope :unliked_by, ->(user) { excluding_user(user).where.not(id: user.likes_from.select(:to_user_id)) } # 利用者がlikeしていないユーザーを取得
+  validates :sub, uniqueness: true
+
+  scope :guest_users, ->(user) { where.not(id: user.id) } # ログインユーザー以外（ゲストユーザー）を取得
+  scope :unliked,     ->(user) { where.not(id: user.likes_from.select(:to_user_id)) } # いいねしていないユーザーを取得
+
+  # いいねしていないゲストユーザー取得
+  def self.unliked_guest_users(user)
+    unliked(user).guest_users(user)
+  end
 
   def self.from_token_payload(payload)
     find_by(sub: payload['sub']) || create!(sub: payload['sub'])
