aac8767cf50c88bd7200940059d0a7d71fd0f764
ryota-sb <ryota.sb.1206@gmail.com>
2023-07-25 00:02:56 +0900

    修正

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.fog_public = false # リソースへの直接アクセスを制限
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'ally-image-bucket'
@@ -16,3 +17,16 @@ CarrierWave.configure do |config|
     config.cache_storage = :file
   end
 end
+
+
+# config.storage = :fog
+    # config.fog_public = false # リソースへの直接アクセスを制限
+    # config.fog_provider = 'fog/aws'
+    # config.fog_directory = 'ally-image-bucket'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ally-image-bucket'
+    # config.fog_credentials = {
+    #   provider: 'AWS',
+    #   aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    #   aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    #   region: 'ap-northeast-1'
+    # }
