46aa9a88048255b45fcd3361105b9614ca8cc00e
ryota-sb <ryota.sb.1206@gmail.com>
2023-07-24 22:12:48 +0900

    修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,8 +3,7 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 if [ "$RAILS_ENV" = "production" ]; then
-bundle exec rails db:migrate
-bundle exec rails db:seed
+bundle exec rails db:reset
 fi
 
 exec "$@"
