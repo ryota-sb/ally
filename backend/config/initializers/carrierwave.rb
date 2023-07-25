5eadae45988b1ac8e67c24486cc61aef5d8274f7
ryota-sb <ryota.sb.1206@gmail.com>
2023-07-25 21:24:12 +0900

    carrierwave 設定ファイル修正

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'ally-image-bucket'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ally-image-bucket'
+    config.asset_host = 'https://ally-image-bucket.s3-ap-northeast-1.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY'],
@@ -12,21 +12,8 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
   else
-    config.asset_host = ENV['CARRIERWAVE_HOST']
+    config.asset_host = "http://backend:3000"
     config.storage = :file
     config.cache_storage = :file
   end
 end
-
-
-# config.storage = :fog
-    # config.fog_public = false # リソースへの直接アクセスを制限
-    # config.fog_provider = 'fog/aws'
-    # config.fog_directory = 'ally-image-bucket'
-    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ally-image-bucket'
-    # config.fog_credentials = {
-    #   provider: 'AWS',
-    #   aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-    #   aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-    #   region: 'ap-northeast-1'
-    # }
