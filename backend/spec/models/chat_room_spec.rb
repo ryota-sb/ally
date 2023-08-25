163e606c9f2b4b9797ef426721a1f923222e2e00
ryota-sb <ryota.sb.1206@gmail.com>
2023-08-26 00:10:51 +0900

    ChatRoomモデルスペック追加

--- backend/spec/models/chat_room_spec.rb
+++ backend/spec/models/chat_room_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ChatRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '関連付け' do
+    let!(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context 'Userモデル' do
+      let!(:target) { :users }
+      it '多対多で紐づいているか' do
+        expect(association.macro).to eq :has_many
+        expect(association.options[:through]).to eq :chat_room_users
+        expect(association.class_name).to eq "User"
+      end
+    end
+
+    context 'Messageモデル' do
+      let!(:target) { :messages }
+      it '1対多で紐づいているか' do
+        expect(association.macro).to eq :has_many
+        expect(association.class_name).to eq "Message"
+      end
+    end
+  end
 end
